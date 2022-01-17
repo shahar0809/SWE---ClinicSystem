5cff8f79ab4fb21d5d7789b6c290339e25c89cfe
Shahar <shahartefler@gmail.com>
2022-01-17 21:53:45 +0200

    Add green pass visualization

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/BaseController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/BaseController.java
@@ -1,6 +1,8 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 import java.io.IOException;
 
@@ -8,5 +10,25 @@ public class BaseController {
     public void signOut(ActionEvent actionEvent) throws IOException {
         App.setActiveUser(null);
         App.setRoot("RegisterLogin");
+    }
+
+    public void informUser(String message) {
+        Alert alert  = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.OK);
+        alert.show();
+    }
+
+    public void alertUserError(String message) {
+        Alert alert  = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+        alert.show();
+    }
+
+    public void confirmUser(String message) {
+        Alert alert  = new Alert(Alert.AlertType.CONFIRMATION, message, ButtonType.OK);
+        alert.show();
+    }
+
+    public void warnUser(String message) {
+        Alert alert  = new Alert(Alert.AlertType.WARNING, message, ButtonType.OK);
+        alert.show();
     }
 }
--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/GreenPassController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/GreenPassController.java
@@ -1,5 +1,46 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-public class GreenPassController extends BaseController {
+import il.cshaifasweng.OCSFMediatorExample.requests.GetGreenPassRequest;
+import il.cshaifasweng.OCSFMediatorExample.response.GetGreenPassResponse;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class GreenPassController extends BaseController {
+    @FXML
+    private ImageView barcode;
+    @FXML
+    private Label date;
+    @FXML
+    private AnchorPane greenPassBoard;
+
+    @FXML
+    public void initialize() {
+        EventBus.getDefault().register(this);
+        greenPassBoard.setVisible(false);
+    }
+
+    public void getGreenPass(ActionEvent actionEvent) {
+        GetGreenPassRequest request = new GetGreenPassRequest(App.getActiveUser());
+        App.getClient().sendRequest(request);
+    }
+
+    @Subscribe
+    public void greenPassResponse(GetGreenPassResponse response) {
+        if (!response.isSuccessful()) {
+            alertUserError(response.getError());
+        } else {
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            greenPassBoard.setVisible(true);
+            date.setText(LocalDate.now().format(format));
+        }
+    }
 }
